35afb379e167336c79746eac2d12d8c58e374395
SnehaSwaroop <swaroop.sn@northeastern.edu>
2021-04-01 20:31:55 -0400

    DB

--- Assignment4/src/Business/DB4OUtil/DB4OUtil.java
+++ Assignment4/src/Business/DB4OUtil/DB4OUtil.java
@@ -62,13 +62,13 @@ public class DB4OUtil {
     
     public EcoSystem retrieveSystem(){
         ObjectContainer conn = createConnection();
-        ObjectSet<EcoSystem> systems = conn.query(EcoSystem.class); // Change to the object you want to save
+        ObjectSet<EcoSystem> ecosystems = conn.query(EcoSystem.class); // Change to the object you want to save
         EcoSystem system;
-        if (systems.size() == 0){
+        if (ecosystems.size() == 0){
             system = ConfigureASystem.configure();  // If there's no System in the record, create a new one
         }
         else{
-            system = systems.get(systems.size() - 1);
+            system = ecosystems.get(ecosystems.size() - 1);
         }
         conn.close();
         return system;
